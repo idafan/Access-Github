7d44474f131e68d62176c454aad4aedd75f4b57d
Ida Fantenberg Niklasson <inik@kth.se>
2018-12-01 21:54:46 +0000

    Updating files to implement codeflower

--- gitAccess/main.js
+++ gitAccess/main.js
@@ -1,2 +1,8 @@
+
+
+
+
+url: 'https://api.github.com/users/'+username
+data:{
 client_id = 'e5de2b9fce0593bf30ec'
-client_secret = '0bd8a6ab845e6bfb029d866988355910fb87c03d'
+client_secret = '0bd8a6ab845e6bfb029d866988355910fb87c03d'}
