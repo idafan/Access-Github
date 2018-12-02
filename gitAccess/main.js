d78412d09726eba453d4d1a72d40699d7bcc136a
Ida Fantenberg Niklasson <inik@kth.se>
2018-12-02 10:55:40 +0000

    Deleting codeflower, adding d3

--- gitAccess/main.js
+++ gitAccess/main.js
@@ -1,8 +1,50 @@
+$(document).ready(function(){
+  $('#searchUser').on('keyup', function(e){
+    let username = e.target.value;
+    let data = {};
+    // Make request to Github
+    $.ajax({
+      url:'https://api.github.com/users/'+username,
+      data:{
+        client_id:'e5de2b9fce0593bf30ec',
+        client_secret:'0bd8a6ab845e6bfb029d866988355910fb87c03d'
+      }
+    }).done(function(user){
+      $.ajax({
+        url:'https://api.github.com/users/'+username+'/repos',
+        data:{
+          client_id:'e5de2b9fce0593bf30ec',
+          client_secret:'0bd8a6ab845e6bfb029d866988355910fb87c03d',
+          sort: 'created: asc',
+          per_page: 20
+        }
+      }).done(function(repos){
+        $.each(repos, function(index, repo){
+          if(repo.language){
+            if(!data[repo.language])
+              data[repo.language]=1;
+            else
+              data[repo.language]+=1;
+          }
+          $('#repos').append(`
+            <div class="well">
+              <div class="row">
+                <div class="col-md-7">
+                  <strong>${repo.name}</strong>:
+                </div>
+                <div class="col-md-3">
+                  <span>${repo.language}</span>
+                </div>
+              </div>
+            </div>
+          `);
+        });
+      });
 
-
-
-
-url: 'https://api.github.com/users/'+username
-data:{
-client_id = 'e5de2b9fce0593bf30ec'
-client_secret = '0bd8a6ab845e6bfb029d866988355910fb87c03d'}
+      $('#profile').html(`
+        <div id="repos"></div>
+      `);
+    });
+    //new jsonData(data);
+  });
+});
---
+++ gitAccess/script.js
@@ -0,0 +1,19 @@
+
+
+var jsonData = function(data){
+  //console.log(data);
+  let json = JSON.stringify(data);
+  console.log(data);
+
+/*
+For username idafan:
+Object
+CSS: 1
+HTML: 2
+JavaScript: 8
+Python: 9
+*/
+
+  //var myFlower = new CodeFlower("#box", 500, 300)
+  //myFlower.update(json);
+};
